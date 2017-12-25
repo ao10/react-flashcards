fb15be77384e56f81c9f9b141d92f0df721b77ba
Ati Ok <ao10@umbc.edu>
2017-12-24 23:42:06 -0500

    Another one.

--- src/App.js
+++ src/App.js
@@ -26,13 +26,17 @@ class App extends Component {
   }
 
   getRandomCard(currentCards){
-    
+    var card = currentCards[Math.floor(Math.random() * currentCards.length)];
+    return(card);
   }
 
   render() {
     return (
       <div className="App">
-        <Card />        
+        <Card eng={this.state.currentCard.eng} 
+              han={this.state.currentCard.han}
+              pin={this.state.currentCard.pin}
+              />        
       </div>
     );
   }
